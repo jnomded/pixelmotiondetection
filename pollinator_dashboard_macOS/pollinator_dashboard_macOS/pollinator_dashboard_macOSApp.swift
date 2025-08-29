b42769eb4bd9ea8433a0b741780bff49bb5048c7
jnomded <james.edmond04@gmail.com>
2025-08-29 12:31:48 -0500

    xcode initialized

---
+++ pollinator_dashboard_macOS/pollinator_dashboard_macOS/pollinator_dashboard_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  pollinator_dashboard_macOSApp.swift
+//  pollinator_dashboard_macOS
+//
+//  Created by James Edmond on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct pollinator_dashboard_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pollinator_dashboard_macOS/pollinator_dashboard_macOSTests/pollinator_dashboard_macOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  pollinator_dashboard_macOSTests.swift
+//  pollinator_dashboard_macOSTests
+//
+//  Created by James Edmond on 8/29/25.
+//
+
+import Testing
+@testable import pollinator_dashboard_macOS
+
+struct pollinator_dashboard_macOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
